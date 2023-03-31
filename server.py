94d62fcf3765b10bd3ca814065d44a06f6bce674
yyang54 <yyang54@ncsu.edu>
2023-03-31 16:03:49 -0400

    1

--- server.py
+++ server.py
@@ -196,7 +196,7 @@ def company_details(company_id):
 def get_company_info(company_id):
     # Fetch the company data using the company ID
     query = "SELECT * FROM company WHERE companyid = :company_id;"
-    result = g.conn.execute(query, company_id=company_id)
+    result = g.conn.execute(query, companyid=company_id)
     company_data = result.fetchone()
 
     # Convert the company data to a dictionary
